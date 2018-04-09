fe0e131a62fcc4751a1eecf560c6213423403129
bstelly <bstelly1999@gmail.com>
2018-04-09 11:47:40 -0500

    initial
    Created a Vector2 class and tested it

---
+++ Vector2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2 testOne = new Vector2(10, 10);
+            Vector2  testTwo = new Vector2(3, 5);
+            Vector2 testThree = new Vector2(10, 10);
+            Vector2 sum = testOne + testTwo;
+            Vector2 diff = testOne - testTwo;
+            Vector2 scale = testTwo * 3;
+            bool compare = testOne == testThree;
+            float result = Vector2.Magnitude(testTwo);
+
+            Vector2.Print(sum);
+            Vector2.Print(diff);
+            Vector2.Print(scale);
+            Console.WriteLine(compare);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vector2/Vector2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector2
+{
+    class Vector2
+    {
+        private float mX;
+        private float mY;
+
+        public Vector2()
+        {
+            mX = 0;
+            mY = 0;
+        }
+        public Vector2(float x, float y)
+        {
+            mX = x;
+            mY = y;
+        }
+
+        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2(lhs.mX + rhs.mX, lhs.mY + rhs.mY);
+        }
+
+        public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2(lhs.mX - rhs.mX, lhs.mY - rhs.mY);
+        }
+
+        public static Vector2 operator *(Vector2 vector, int scale)
+        {
+            return new Vector2(vector.mX * scale, vector.mY * scale);
+        }
+
+        public static bool operator ==(Vector2 lhs, Vector2 rhs)
+        {
+            return (lhs.mX == rhs.mX && lhs.mY == rhs.mY);
+        }
+
+        public static bool operator !=(Vector2 lhs, Vector2 rhs)
+        {
+            return (lhs.mX != rhs.mX && lhs.mY != rhs.mY);
+        }
+
+        public static float Dot(Vector2 lhs, Vector2 rhs)
+        {
+            return (lhs.mX * rhs.mX) + (lhs.mY * rhs.mY);
+        }
+
+        public static float Magnitude(Vector2 vector)
+        {
+            float result = (float) Math.Sqrt((vector.mX * vector.mX) + (vector.mY * vector.mY));
+            return result;
+        }
+
+        public static Vector2 Normalize(Vector2 vector)
+        {
+            return new Vector2(vector.mX / Magnitude(vector), vector.mY / Magnitude(vector));
+        }
+
+        public static void Print(Vector2 vector)
+        {   Console.Write("<");
+            Console.Write(vector.mX);
+            Console.Write(", ");
+            Console.Write(vector.mY);
+            Console.WriteLine(">");
+        }
+    }
+}
