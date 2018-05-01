a3cc438c81d846c800b787227c375f6119a15eb7
bstelly <bstelly1999@gmail.com>
2018-05-01 15:39:47 -0500

    Fixed Buttons

--- RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.RockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.RockButton.Location = new System.Drawing.Point(12, 12);
             this.RockButton.Name = "RockButton";
-            this.RockButton.Size = new System.Drawing.Size(65, 65);
+            this.RockButton.Size = new System.Drawing.Size(70, 70);
             this.RockButton.TabIndex = 0;
             this.RockButton.Text = "Rock";
             this.RockButton.UseVisualStyleBackColor = true;
@@ -49,9 +49,9 @@
             // PaperButton
             // 
             this.PaperButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PaperButton.Location = new System.Drawing.Point(12, 101);
+            this.PaperButton.Location = new System.Drawing.Point(12, 97);
             this.PaperButton.Name = "PaperButton";
-            this.PaperButton.Size = new System.Drawing.Size(65, 65);
+            this.PaperButton.Size = new System.Drawing.Size(70, 70);
             this.PaperButton.TabIndex = 1;
             this.PaperButton.Text = "Paper";
             this.PaperButton.UseVisualStyleBackColor = true;
@@ -69,10 +69,10 @@
             // 
             // ScissorsButton
             // 
-            this.ScissorsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ScissorsButton.Location = new System.Drawing.Point(12, 188);
+            this.ScissorsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScissorsButton.Location = new System.Drawing.Point(12, 183);
             this.ScissorsButton.Name = "ScissorsButton";
-            this.ScissorsButton.Size = new System.Drawing.Size(65, 65);
+            this.ScissorsButton.Size = new System.Drawing.Size(70, 70);
             this.ScissorsButton.TabIndex = 3;
             this.ScissorsButton.Text = "Scissors";
             this.ScissorsButton.UseVisualStyleBackColor = true;
