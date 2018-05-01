0cd923356e8fa9cd63da2a79fce2a38ccb053c80
bstelly <bstelly1999@gmail.com>
2018-05-01 17:59:20 -0500

    backpack asssignmet
    Began making classes and interfaces

---
+++ BackpackWinform/Backpack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackpackWinform
+{
+    class Backpack
+    {
+        public List<IPackable> Packables;
+
+        public Backpack(List<IPackable> packablesList)
+        {
+            Packables = packablesList;
+        }
+    }
+}
---
+++ BackpackWinform/IPackable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackpackWinform
+{
+    interface IPackable
+    {
+        string Name { get; set; }
+
+        bool PickedUp { get; set; }
+
+        void Pack(Backpack backpack);
+    }
+}
